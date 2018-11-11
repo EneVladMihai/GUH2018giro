fe32ca77ffeab2853205da002dd2b8c719349b6c
Vlad <vlad-mihai.ene@student.manchester.ac.uk>
2018-11-11 07:56:51 +0000

    logic done

--- app/src/main/java/com/example/enevl/guh2018gyroscop/MainActivity.java
+++ app/src/main/java/com/example/enevl/guh2018gyroscop/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.enevl.guh2018gyroscop;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -11,6 +15,13 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.Set;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
@@ -20,6 +31,11 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private int futureStandardX, standardX;
     private Button calobrateButton;
     private TextView xtextView, ytextView, ztextView;
+    BluetoothAdapter bluetoothAdapter;
+    BluetoothDevice device;
+    BluetoothSocket socket;
+    OutputStream outputStream;
+    private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
 
     private int move, angle;
 
@@ -43,6 +59,49 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                 standardX = futureStandardX;
             }
         });
+
+        bluetoothAdapter=BluetoothAdapter.getDefaultAdapter();
+
+        if (bluetoothAdapter == null) {
+
+            Toast.makeText(getApplicationContext(),"Device doesnt Support Bluetooth",Toast.LENGTH_SHORT).show();
+
+        }
+        if(!bluetoothAdapter.isEnabled())
+
+        {
+
+            Intent enableAdapter = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+
+            startActivityForResult(enableAdapter, 0);
+
+        }
+        Set<BluetoothDevice> bondedDevices = bluetoothAdapter.getBondedDevices();
+
+        if(bondedDevices.isEmpty()) {
+
+            Toast.makeText(getApplicationContext(),"Please Pair the Device first",Toast.LENGTH_SHORT).show();
+
+        } else {
+
+            for (BluetoothDevice iterator : bondedDevices) {
+                {
+
+                    device=iterator; //device is an object of type BluetoothDevice
+                    break;
+
+                } } }
+        try {
+            socket = device.createRfcommSocketToServiceRecord(MY_UUID); socket.connect();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            outputStream=socket.getOutputStream();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -97,6 +156,16 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             xtextView.setText("MOVE: " + move);
             ytextView.setText("Angle: " + angle);
             //ztextView.setText("Z: " + orientations[2]);
+            try {
+                outputStream.write(angle);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                outputStream.write(move);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
